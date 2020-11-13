f1a80a8c62d18b06579a16682051aaa06baea7de
tienpham <tiens8acc@enjoyworks.co.kr>
2020-11-13 10:38:12 +0700

    cm

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-	// $ems= \DB::select('select * from employees');
-	$ems= \DB::select('select * from departments');
+	$ems= \DB::select('select * from employees');
+	// $ems= \DB::select('select * from departments');
+	// $ems= \DB::select('select * from dept_manager');
+	// $ems= \DB::select('select * from dept_emp');
+	// $ems= \DB::select('select * from titles');
+	// $ems= \DB::select('select * from salaries');
 	dd($ems);
     return 'test';
 });
